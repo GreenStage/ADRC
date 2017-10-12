0f12ad4bd4b2f6a3735d8807ecebface5ae3e1dc
TugaTyon <j040.silva@gmail.com>
2017-10-12 19:52:00 +0100

    mexi apenas no temp.c check DeletePrefix pl0x

--- temp.c
+++ temp.c
@@ -7,3 +7,55 @@ void PrintTable(Tree * root){
     printf("%d",ptr->hopNumber);
 
 }
+
+
+Tree * DeletePrefix(Tree * prefixTree, char * address){
+    static int i = 0;
+    //int retval;
+    static Tree_Node * it = NULL;
+    //Tree_Node * ptr;
+  
+    //pra que raio usas isto?
+    ASSERT_RETR(i > (int) strlen(address) - 1, -1);
+  
+    if(it == NULL){
+      it = prefixTree->root;
+    } 
+  
+    ptr = it;
+   
+
+    for(i = 0; i < (int) strlen(address); i++){
+        
+        switch(address[i]){
+            case '0':
+            if(!it->left){
+                //calhou coco return it->nextHop;
+            }
+            it= it->left;
+            break;
+            case '1':
+            if(!it->right){
+                //calhou coco return it->nextHop;
+            }
+            it= it->right;
+            break;
+            default:
+            return NULL;
+        
+        }
+    
+    }
+//it now points to the one that shall be removed
+    if(!it->left && !it->right){
+        //this node has no children, it can be FREE
+        free(it);//CHECK
+
+    }else{
+        //this node has some children, it needs to be here for them
+        it->nextHop = 0;
+    }
+    
+  
+    return prefixTree;
+}  
